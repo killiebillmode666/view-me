087c8a9e6dd0f8868a9ccc8941f1ee034e86eb5f
killiebillmode666 <145313108+killiebillmode666@users.noreply.github.com>
2023-09-18 14:27:59 +1000

    Update viewer_simulator.py

--- viewer_simulator.py
+++ viewer_simulator.py
@@ -1,13 +1,32 @@
 import random
 import time
+import requests  # Import the requests library for HTTP requests.
 
-def increase_viewers(stream_url):
+# Function to update viewer count via the streaming platform's API.
+def update_viewer_count(api_url):
+    try:
+        response = requests.get(api_url)
+        if response.status_code == 200:
+            viewer_data = response.json()
+            return viewer_data['viewer_count']
+        else:
+            print(f"Failed to fetch viewer count. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error updating viewer count: {str(e)}")
+
+def increase_viewers(stream_url, api_url):
     while True:
         fake_viewers = random.randint(1, 10)  # Simulate viewers joining your stream.
         print(f"New viewers joined: {fake_viewers}")
-        # Add code to interact with the streaming platform's API to update viewer count.
+        
+        # Update viewer count via the streaming platform's API.
+        viewer_count = update_viewer_count(api_url)
+        if viewer_count is not None:
+            print(f"Current viewer count: {viewer_count}")
+        
         time.sleep(random.randint(10, 60))  # Simulate random viewer activity.
 
 if __name__ == "__main__":
     stream_url = "your_stream_url_here"
-    increase_viewers(stream_url)
+    api_url = "https://api.streaming-platform.com/viewer_count"  # Replace with the actual API endpoint.
+    increase_viewers(stream_url, api_url)
